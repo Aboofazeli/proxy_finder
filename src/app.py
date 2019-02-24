4e61a1f185b582e1952c1340cdf2690a764c4dc6
alireza822 <alireza822@gmail.com>
2019-02-24 01:33:55 -0500

    make it better2

--- src/app.py
+++ src/app.py
@@ -126,6 +126,6 @@ def convert_to_dataframe(final_result):
 def index():
     return render_template('index.html')
 
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run()
